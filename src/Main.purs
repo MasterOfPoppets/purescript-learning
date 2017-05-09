07d0f0fdd3530cf4b410c020e3291e9132f60656
Gareth Hughes <gurrkin@googlemail.com>
2017-05-09 22:40:02 +0100

    Godliness

--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,17 @@ module Main where
 import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Foreign (F, Foreign, ForeignError(..), fail, readInt, readNull, tagOf, unsafeFromForeign)
+import Data.Foreign (F, Foreign, ForeignError(..), fail, readInt, readNull, readNullOrUndefined, tagOf, unsafeFromForeign)
 import Data.Foreign.Index (readProp)
 import Data.Maybe (Maybe(..), maybe)
 import Data.Traversable (traverse)
 
 data MiddleRail = OneHundred | TwoHundred | TwoHundredAndFifty
 
+type SashHeight = Int
+
 newtype Door = Door {
-  bob :: Int,
+  sashHeight :: SashHeight,
   middleRail :: Maybe MiddleRail
 }
 
@@ -24,16 +26,21 @@ readMiddleRail value
 
 readDoor :: Foreign -> F Door
 readDoor value = do 
-  bob <- readInt =<< readProp "bob" value
-  middleRail <- traverse readMiddleRail =<< readNull =<< readProp "middleRail" value
-  pure $ Door { bob, middleRail }
+  sashHeight <- readInt =<< readProp "sashHeight" value
+  middleRail <- traverse readMiddleRail =<< readNullOrUndefined =<< readProp "middleRail" value
+  pure $ Door { sashHeight, middleRail }
 
-isValidMiddleRail :: MiddleRail -> Boolean
-isValidMiddleRail OneHundred = true
-isValidMiddleRail _ = false
+isValidMiddleRail :: SashHeight -> MiddleRail -> Boolean
+isValidMiddleRail height OneHundred
+  | height >= 2100 = true
+  | otherwise = false
+isValidMiddleRail height TwoHundred
+  | height >= 2200 = true 
+  | otherwise = false
+isValidMiddleRail _ _ = false
 
 isValidDoor :: Door -> Boolean 
-isValidDoor (Door door) = maybe false isValidMiddleRail door.middleRail
+isValidDoor (Door door) = maybe false (isValidMiddleRail door.sashHeight) door.middleRail 
 
 isValidDoorJS :: Foreign -> F Boolean
 isValidDoorJS foreignDoor = isValidDoor <$> (readDoor foreignDoor)
@@ -44,10 +51,10 @@ getMiddleRailHeight TwoHundred = 200
 getMiddleRailHeight TwoHundredAndFifty = 250
 
 testDoor1 :: Door
-testDoor1 = Door { bob: 100, middleRail: Just TwoHundred}
+testDoor1 = Door { sashHeight: 2300, middleRail: Just TwoHundred}
 
 testDoor2 :: Door 
-testDoor2 = Door { bob: 250, middleRail : Nothing }
+testDoor2 = Door { sashHeight: 2100, middleRail : Nothing }
 
 showDoor :: Door -> String
 showDoor (Door door) = "Door " <> show (getMiddleRailHeight <$> door.middleRail)
--- test.js
+++ test.js
@@ -1,13 +1,7 @@
-const Data_Maybe = require('./output/Data.Maybe/index')
-const {
-  isValidDoorJS,
-  getMiddleRailHeight,
-  door,
-  TwoHundredAndFifty
-} = require('./output/Main/index')
+const { isValidDoorJS } = require('./output/Main/index')
 
 const testDoor = {
-  bob: 250,
+  sashHeight: 2150,
   middleRail: 100
 }
 

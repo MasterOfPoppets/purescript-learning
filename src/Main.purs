751dc4fc9f1d3940d70f9df5e712ba449fd3cc0a
Gareth Hughes <gurrkin@googlemail.com>
2017-05-10 22:25:53 +0100

    meh

--- src/Main.purs
+++ src/Main.purs
@@ -1,21 +1,48 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Foreign (F, Foreign, ForeignError(..), fail, readInt, readNull, readNullOrUndefined, tagOf, unsafeFromForeign)
+import Data.Foreign (F, Foreign, ForeignError(..), fail, readArray, readInt, readNullOrUndefined, tagOf, unsafeFromForeign)
 import Data.Foreign.Index (readProp)
-import Data.Maybe (Maybe(..), maybe)
+import Data.Maybe (Maybe, maybe)
 import Data.Traversable (traverse)
 
 data MiddleRail = OneHundred | TwoHundred | TwoHundredAndFifty
 
+data TopRail = Standard | Compact
+
+type BottomRail = Int
 type SashHeight = Int
 
-newtype Door = Door {
-  sashHeight :: SashHeight,
-  middleRail :: Maybe MiddleRail
+newtype Panel = Panel {
+  x :: Int, 
+  y :: Int
 }
+
+newtype Middle = Middle {
+  panels :: Array Panel,
+  rail :: MiddleRail
+}
+
+newtype Door = Door {
+  bottomRail :: BottomRail,
+  sashHeight :: SashHeight,
+  middle :: Maybe Middle,
+  topRail :: TopRail
+}
+
+readDoor :: Foreign -> F Door
+readDoor value = do 
+  bottomRail <- readProp "bottomRail" value >>= readInt
+  middle <- readProp "middle" value >>= readNullOrUndefined >>= traverse readMiddle
+  sashHeight <- readProp "sashHeight" value >>= readInt
+  topRail <- readProp "topRail" value >>= readTopRail
+  pure $ Door { bottomRail, middle, sashHeight, topRail }
+
+readMiddle :: Foreign -> F Middle 
+readMiddle value = do 
+  panels <- readProp "panels" value  >>= readArray >>= traverse readPanel
+  rail <- readProp "rail" value >>= readMiddleRail
+  pure $ Middle { panels, rail }
 
 readMiddleRail :: Foreign -> F MiddleRail
 readMiddleRail value
@@ -24,11 +51,26 @@ readMiddleRail value
   | unsafeFromForeign value == 250 = pure TwoHundredAndFifty
   | otherwise = fail $ TypeMismatch "MiddleRail" (tagOf value)
 
-readDoor :: Foreign -> F Door
-readDoor value = do 
-  sashHeight <- readInt =<< readProp "sashHeight" value
-  middleRail <- traverse readMiddleRail =<< readNullOrUndefined =<< readProp "middleRail" value
-  pure $ Door { sashHeight, middleRail }
+readPanel :: Foreign -> F Panel 
+readPanel value = do 
+  x <- readProp "x" value >>= readInt
+  y <- readProp "y" value  >>= readInt
+  pure $ Panel { x, y }
+
+readTopRail :: Foreign -> F TopRail
+readTopRail value 
+  | unsafeFromForeign value == "Standard" = pure Standard 
+  | unsafeFromForeign value == "Compact" = pure Compact
+  | otherwise = fail $ TypeMismatch "TopRail" (tagOf value)
+
+isValidRails :: BottomRail -> TopRail -> Boolean 
+isValidRails _ Standard = true 
+isValidRails bottomRail Compact 
+  | bottomRail >= 100 = true 
+  | otherwise = false
+
+isValidMiddle :: SashHeight -> Middle -> Boolean 
+isValidMiddle height (Middle middle) = isValidMiddleRail height middle.rail
 
 isValidMiddleRail :: SashHeight -> MiddleRail -> Boolean
 isValidMiddleRail height OneHundred
@@ -40,7 +82,9 @@ isValidMiddleRail height TwoHundred
 isValidMiddleRail _ _ = false
 
 isValidDoor :: Door -> Boolean 
-isValidDoor (Door door) = maybe false (isValidMiddleRail door.sashHeight) door.middleRail 
+isValidDoor (Door door) = validMiddle && validRails
+  where validMiddle = maybe false (isValidMiddle door.sashHeight) door.middle
+        validRails = isValidRails door.bottomRail door.topRail
 
 isValidDoorJS :: Foreign -> F Boolean
 isValidDoorJS foreignDoor = isValidDoor <$> (readDoor foreignDoor)
@@ -49,17 +93,3 @@ getMiddleRailHeight :: MiddleRail -> Int
 getMiddleRailHeight OneHundred = 100
 getMiddleRailHeight TwoHundred = 200
 getMiddleRailHeight TwoHundredAndFifty = 250
-
-testDoor1 :: Door
-testDoor1 = Door { sashHeight: 2300, middleRail: Just TwoHundred}
-
-testDoor2 :: Door 
-testDoor2 = Door { sashHeight: 2100, middleRail : Nothing }
-
-showDoor :: Door -> String
-showDoor (Door door) = "Door " <> show (getMiddleRailHeight <$> door.middleRail)
-
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log $ showDoor testDoor1
-  log $ showDoor testDoor2
--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const { isValidDoorJS } = require('./output/Main/index')
 
 const testDoor = {
+  bottomRail: 100,
+  middle: {
+    panels: [{ x: 100, y: 220 }, { x: 200, y: 300 }],
+    rail: 100
+  },
   sashHeight: 2150,
-  middleRail: 100
+  topRail: 'Standard'
 }
 
 console.log(isValidDoorJS(testDoor))
